6215131c9463fe63061c0b6be8a96ea9081bc3d1
Shadajifrin <fathimashadanp@gmail.com>
2023-12-01 14:40:29 +0530

    add logo image

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ function App() {
 
   return (
    <div className='login'>
-    
+      <img src= "assets/Logo.svg"></img>
     <form>
+     
       <div className='container'>
       <h1>Login as admin</h1>
-       <label className='username'>Username</label>
+     
+      <label className='username'>Username</label>
       <input type='text' onChange={(e)=>{setUsername(e.target.value)}} placeholder='Username' />
       <label className='password'>Password</label>
       <input type='password' onChange={(e)=>{setPassword(e.target.value)}} placeholder='Password'/>
      <button type='submit' >Login</button>
       </div>
+      
     </form>
 
    </div>
