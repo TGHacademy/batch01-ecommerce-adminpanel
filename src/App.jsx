e069bae9092f9e28d0288ef8b84889d03e12a07b
Shadajifrin <fathimashadanp@gmail.com>
2023-12-01 20:47:24 +0530

    add logo , image properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import logo from "/src/assets/Logo.svg"
+import bgimage from "/src/assets/Frame 160.svg"
 
 function App() {
   const [count, setCount] = useState()
 
   return (
    <div className='login'>
-      <img src= "assets/Logo.svg"></img>
+    <img src={bgimage}  />
     <form>
      
       <div className='container'>
+      <img src= {logo}/>
       <h1>Login as admin</h1>
+
      
       <label className='username'>Username</label>
       <input type='text' onChange={(e)=>{setUsername(e.target.value)}} placeholder='Username' />
