8496e1b408106f443aca16d0f1a50f2e57163fb0
Shadajifrin <fathimashadanp@gmail.com>
2023-12-04 08:38:54 +0530

    add form validation

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,54 @@ import logo from "/src/assets/Logo.svg"
 import bgimage from "/src/assets/Frame 160.svg"
 
 function App() {
-  const [count, setCount] = useState()
+  const [username, setUsername] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  
 
+  const handleUsernameChange=(e)=>{
+    setSuccessMsg('');
+    setUsernameError('');
+    setUsername(e.target.value);
+  }
+  const handlePasswordChange=(e)=>{
+    setSuccessMsg('');
+    setPasswordError('');
+    setPassword(e.target.value);
+  }
+  const handleFormSubmit=(e)=>{
+    e.preventDefault();
+
+    if(username!==''){
+
+    }
+    else{
+      setUsernameError('Username Required');
+    }
+    if(password!==''){
+
+    }
+    else{
+      setPasswordError('Password Required');
+    }
+
+  }
   return (
    <div className='login'>
     <img src={bgimage}  />
-    <form>
-     
+    <form onSubmit={handleFormSubmit}>
       <div className='container'>
       <img src= {logo}/>
       <h1>Login as admin</h1>
 
      
       <label className='username'>Username</label>
-      <input type='text' onChange={(e)=>{setUsername(e.target.value)}} placeholder='Username' />
+      <input type='text' onChange={handleUsernameChange} placeholder='Username' />
+      {usernameError&&<div  className='error-msg'>{usernameError}</div>}
       <label className='password'>Password</label>
-      <input type='password' onChange={(e)=>{setPassword(e.target.value)}} placeholder='Password'/>
+      <input type='password' onChange={handlePasswordChange} placeholder='Password'/>
+      {passwordError&&<div  className='error-msg'>{passwordError}</div>}
      <button type='submit' >Login</button>
       </div>
       
