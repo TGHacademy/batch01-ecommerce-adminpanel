f84f3dce895d4e2a4d66eb897f8945c0eb4acba0
Shadajifrin <fathimashadanp@gmail.com>
2023-12-13 14:19:07 +0530

    api integrate,toast msg add

--- src/App.jsx
+++ src/App.jsx
@@ -2,64 +2,101 @@ import { useState } from 'react'
 import './App.css'
 import logo from "/src/assets/Logo.svg"
 import bgimage from "/src/assets/Frame 160.svg"
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   const [username, setUsername] = useState('');
   const [usernameError, setUsernameError] = useState('');
   const [password, setPassword] = useState('');
-  const[message,setMessage]=useState('');
-  
+  const [passwordError, setPasswordError] = useState('');
+ 
   
   const handleUsernameChange=(e)=>{
     setUsernameError('');
     setUsername(e.target.value);
   }
   const handlePasswordChange=(e)=>{ 
-   
+    setPasswordError('');
     setPassword(e.target.value);
   }
   const handleFormSubmit=(e)=>{
-    const regExp=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
     e.preventDefault();
 
-    if(username!==''){
-
-    }
+    if(username!==''){ /* empty */ }
     else{
-      setUsernameError('Username Required');
+      setUsernameError('Please enter username');
     }
-    if(password ===''){
-      setMessage("Please enter password");
-    }
-    else if(regExp.test(password)){
-      setMessage("Password is Valid");
-    }
-    else if(!regExp.test(password)){
-      setMessage("Password is  not Valid");
-      
+    if(password !==''){ /* empty */ }
+    else
+    {
+      setPasswordError("Please enter password");
     }
 
   }
+
+  
+  // useEffect(() => {
+  //   const fetchLink = async () => {
+  //     try {
+  //       const response = await fetch('https://academy-batch-1-project-683989f58497.herokuapp.com/');
+  //       if (response.ok) {
+  //         const linkData = await response.json();
+  //         setApiLink(linkData.link);
+  //       } else {
+  //         console.error('Failed to fetch link from backend');
+  //       }
+  //     } catch (error) {
+  //       console.error('Error during link fetch:', error);
+  //     }
+  //   };
+  //   fetchLink();
+  // }, []); 
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('https://academy-batch-1-project-683989f58497.herokuapp.com/api/auth/admin-login', {
+        
+      method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const data= await response.json();
+      console.log(data);
+      if (response.ok) {
+        localStorage.setItem("Token",JSON.stringify(data.token))
+        console.log('Login successful!');
+        toast.success('Successfully Login!');
+      } else {
+         console.error('Login failed');
+         toast.error(data.error);
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      
+    }
+  };
   return (
    <div className='login'>
     <img src={bgimage}  />
     <form onSubmit={handleFormSubmit}>
       <div className='container'>
-      <img src= {logo}/>
+      <img src= {logo}/> 
       <h1>Login as admin</h1>
 
      
       <label className='username'>Username</label>
-      <input type='text' onChange={handleUsernameChange} placeholder='Username' />
+      <input type='email' onChange={handleUsernameChange} placeholder='Username' />
       {usernameError&&<div  className='error-msg'>{usernameError}</div>}
       <label className='password'>Password</label>
       <input type='password' onChange={handlePasswordChange} placeholder='Password'/>
-     <p className='error-msg'>{message}</p>
-     <button type='submit' >Login</button>
+      {passwordError&&<div  className='error-msg'>{passwordError}</div>}
+     <button  onClick={handleLogin}type='submit' >Login</button>
       </div>
       
     </form>
-
+    <Toaster />
    </div>
   )
 }
